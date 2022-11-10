dbbdfd922ae0e99a1723316a0c32d85356728879
Superintendent-Vergil <jpfarrel@lakeheadu.ca>
2022-11-10 18:14:24 -0500

    Made Switch case for selecting Algorithm

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 async function main(text, hashType){
-    let result;
-    result = await SHA1HashStr(text);
-    console.log(result);
-    document.getElementById("hashOutput").innerHTML = result;
+    let hashResult; //Initailize result string
+    console.log(hashType)
+    switch (hashType) { //Select hash function to run based on hashType selected input
+        case 'SHA1': hashResult = await SHA1HashStr(text); break;
+        case 'SHA256': hashResult = "TODO: Implement SHA256"; break;
+        default: hashResult = "Error, invalid Algorithm Selection" //Returns error message if hashType invalid
+    }
+    //console.log(result);
+    document.getElementById("hashOutput").innerHTML = hashResult;
 }
