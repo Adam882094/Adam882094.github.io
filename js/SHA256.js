776830e23fbbd425f3b59e42302825385e886095
Adam Brennan <atbrenna@lakeheadu.ca>
2022-11-11 14:35:34 -0500

    SHA-256 added

---
+++ js/SHA256.js
@@ -0,0 +1,7 @@
+async function SHA256HashStr(str) { //JPF1106768 - 2022-11-10
+    var strIntArr = new TextEncoder().encode(str); //encode as Uint8Array
+    var buffer = await crypto.subtle.digest('SHA-256', strIntArr); //hash the string
+    var hashArr = Array.from(new Uint8Array(buffer)); //convert buffer to byte array
+    console.log("test");
+    return hexStr = hashArr.map((b) => b.toString(16).padStart(2, '0')).join(''); //convert bytes to hex string
+}
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ async function main(text, hashType){
     console.log(hashType)
     switch (hashType) { //Select hash function to run based on hashType selected input
         case 'SHA1': hashResult = await SHA1HashStr(text); break;
-        case 'SHA256': hashResult = "TODO: Implement SHA256"; break;
+        case 'SHA256': hashResult = await SHA256HashStr(text); break;
         default: hashResult = "Error, invalid Algorithm Selection" //Returns error message if hashType invalid
     }
     console.log(hashResult);
