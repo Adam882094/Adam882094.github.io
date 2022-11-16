a3256ab98524c827886d960c6d50acc40c2f5df8
Akeem Palmer <akeemk.palmer@gmail.com>
2022-11-16 00:21:54 -0500

    Update: UX and added custom salt functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 async function main(text, hashType) {
   let hashResult; //Initailize result string
   console.log(hashType);
+
+  // Clear the text if something is already there.
+  if (document.getElementById("hashOutput").innerHTML != "") {
+    document.getElementById("hashOutput").innerHTML = "";
+  }
+  // Show spinner for 2 seconds then remove and show text.
+  loadSpinnerHandler("start");
+
+  // Check if salt option is checked.
+  if (document.getElementById("saltCheck").checked) {
+    text = saltTextHandler(text);
+  }
+
   switch (
     hashType //Select hash function to run based on hashType selected input
   ) {
@@ -20,5 +33,48 @@ async function main(text, hashType) {
       hashResult = "Error, invalid Algorithm Selection"; //Returns error message if hashType invalid
   }
   console.log(hashResult);
-  document.getElementById("hashOutput").innerHTML = hashResult;
+
+  // Replicate a async action
+  setTimeout(() => {
+    loadSpinnerHandler("stop");
+    document.getElementById("hashOutput").innerHTML = hashResult;
+  }, 3000);
+}
+
+// load the spinner
+function loadSpinnerHandler(action) {
+  if (action === "stop") {
+    const loaders = document.querySelectorAll(".loadingStart");
+    loaders.forEach((item) => {
+      item.classList.replace("loadingStart", "loadingStop");
+    });
+  }
+
+  if (action === "start") {
+    const loaders = document.querySelectorAll(".loadingStop");
+    loaders.forEach((item) => {
+      item.classList.replace("loadingStop", "loadingStart");
+    });
+  }
+}
+
+// Custom Salt function
+function saltTextHandler(text) {
+  saltedText = generateSalt(15) + text + generateSalt(15);
+
+  return saltedText;
+}
+
+// Helper function
+function generateSalt(length) {
+  // declare all characters
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
